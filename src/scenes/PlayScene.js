c18c3a6113c57febf05efdefa02306d4ce2c8f2f
UmezeyiGW <umezeyi@greatwave.ai>
2024-10-13 23:22:25 +0100

    Play Scene Background
    Adding the static tiled background.

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -1,5 +1,6 @@
 import { Scene } from "phaser";
 import TilemapBuilder from "../bg/TilemapBuilder";
+import Scenery from "../bg/Scenery";
 
 export class PlayScene extends Scene {
 
@@ -15,6 +16,17 @@ export class PlayScene extends Scene {
     camera.setBounds(0, 0, width, camera.height);
     camera.centerOn(width * .5, camera.height / 2);
 
+    //  Groups and layers
+    
+    const allGroup = this.add.group({
+      runChildUpdate: true
+    });
+    const allLayer = this.add.layer();
+
+    // Background scene
+
+    const scenery = new Scenery(this);
+    scenery.addFullScene(allLayer, allGroup);
 
     //  Tilemap
 
@@ -22,5 +34,8 @@ export class PlayScene extends Scene {
     const tmBuilder = new TilemapBuilder(this);
     tmBuilder.buildTilemap(bg_tilemap);
 
+    //  Add some buildings
+    //  TexturePacker - Buildings
+    //  JSON layout file
   }
 }
