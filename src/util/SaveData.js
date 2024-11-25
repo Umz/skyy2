24df31d7d4cb3b9a048553c03510eefe7bb3fff5
UmezeyiGW <umezeyi@greatwave.ai>
2024-11-25 09:44:45 +0000

    Static SaveData
    Static class for saving and loading data-

---
+++ src/util/SaveData.js
@@ -0,0 +1,15 @@
+import localforage from "localforage";
+
+export default class SaveData {
+
+  
+
+  static SETUP_LOCALFORAGE_CONFIG() {
+    localforage.config({
+      name: "skyy2-dev1",
+      description: "Data store for Skyy2",
+      storeName: "skydata"
+    })
+  }
+
+}
