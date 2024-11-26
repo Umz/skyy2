2a1eeeb6c0a76db65e2785dc7a3e71fc0a09bbf0
UmezeyiGW <umezeyi@greatwave.ai>
2024-11-26 08:31:22 +0000

    Create Story.js
    Creating the class that will show the story-

---
+++ src/util/Story.js
@@ -0,0 +1,22 @@
+/*
+
+Part 0: Introduction.
+
+After his heroic feats during the Battle of New Moon, Moon Chief was appointed Chief in Command of Moon at Midnight. He was celebrated by the tribe for his courage and might.
+
+Strictly visual
+
+Part 1: Protect the land.
+
+There are still enemies in the region who dare to attack and raid our tribe.
+Destroy them completely, until just the mention of Moon Chief will be our protection.
+Be ruthless.
+
+Find and destroy clusters of enemies.
+
+
+// Show this text on screen
+// Darken the screen and stop the game flow while story is showing?
+// Move along the game progress from 0 (Introduction) to 1 (Enemies attacking)
+
+*/
