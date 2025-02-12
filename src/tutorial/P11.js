a71e6645894222f8282f4c14d6b2a7225d085392
UmezeyiGW <umezeyi@greatwave.ai>
2025-02-12 22:58:55 +0000

    Sequences Complete.
    Gone through the skeletons of the sequences. Start till end.

--- src/tutorial/P11.js
+++ src/tutorial/P11.js
@@ -14,21 +14,42 @@ export default class P11 extends TutorialSequence {
     const { scene } = this;
     const player = this.scene.player;
     const script = Subtitles.GetScript();
+    const WIDTH = Vars.AREA_WIDTH;
 
     this
     .addStopSaving()  // Temp (dev)
 
     .addInstruction(Instructions.P11_CONQUER)
-    // Claim the forest
 
-    // Claim the village
+    .addTitle(" >>> Spawn a claimer flag for Greenleaf forest")
 
-    // Stick around to build Storm
-    // Talk to NPCs
-    // Collect rocks
+    .add(()=>{
+      this.doOnce(()=>{
+        SequenceHelper.SpawnClaimerFlag(WIDTH * 6.5);
+      });
+      return SaveData.Data.claimed.includes(Enum.LOC_GREEN_FOREST);
+    })
+    .addSave()
+    .addWait(4000)
+    .addSpeakerAndWait(player, Icon.BANNER, "The village is next.", 5000)
 
-    // Game is over- just allow roaming.
+    .addTitle(" >>> Spawn a claimer flag for Green village")
 
+    .add(()=>{
+      this.doOnce(()=>{
+        SequenceHelper.SpawnClaimerFlag(WIDTH * 6.5);
+      });
+      return SaveData.Data.claimed.includes(Enum.LOC_GREEN_FOREST);
+    })
+    .addSave()
+    .addWait(4000)
+
+    .addSpeakerAndWait(player, Icon.BANNER, "All the land belongs to Moon at Midnight!", 5000)
+
+    .addInstruction(Instructions.P11_COMPLETE)
+    .addSave()
+
+    .add(()=> false)
   }
 
 }
